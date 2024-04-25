913138ef38ffa2a6b0671b5d8f5549ba0578f17f
joric.hantzberg <joric.hantzberg@epita.fr>
2024-04-25 14:26:54 +0200

    color changes

--- home/tailwind.config.js
+++ home/tailwind.config.js
@@ -3,9 +3,9 @@ module.exports = {
   content: ['./templates/**/*.html', './home/templates/**/*.html','./home/static/src/**/*.css'],
   theme: {
     colors: {
-      'background': '#f2ecce',
+      'background': '#f2f2f2',
       'button': '#cbd966',
-      'main': '#5da6a6',
+      'main': '#733f5c',
       'footer': '#cbd966',
       'white': '#ffffff',
       'black': '#000000',
